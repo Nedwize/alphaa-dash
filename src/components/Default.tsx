3147fd406b0b801a0fb0d5e091137401e8987b3e
NakshatraCodes <nakshatra163@gmail.com>
2021-06-21 19:22:20 +0530

    added header to default dashboard page

--- src/components/Default.tsx
+++ src/components/Default.tsx
@@ -1,31 +1,36 @@
-import React from "react";
-
+import React, { useState } from 'react';
+import useStyles from '../custom-hooks/useStyles';
+import style from '../assets/style';
+import { HeaderComponent } from '.';
 function DashboardsList(props: any) {
+  const user = JSON.parse(sessionStorage.getItem('userData') || '');
+  const classes = useStyles(style)();
+  const [openTask, setOpenTask] = useState(false);
   const { dashboards, onDelete, onAdd } = props;
 
   const getDashboard = (dashboard: any) => (
     <div
       style={{
-        display: "inline-block",
+        display: 'inline-block',
         width: 250,
         height: 100,
-        border: "3px solid black",
+        border: '3px solid black',
         margin: 20,
       }}
     >
-      <div style={{ display: "inline-block", padding: 40 }}>
-        <div style={{ display: "flex" }}>
-          <div style={{ display: "flex", flexGrow: 1, fontWeight: "bold" }}>
+      <div style={{ display: 'inline-block', padding: 40 }}>
+        <div style={{ display: 'flex' }}>
+          <div style={{ display: 'flex', flexGrow: 1, fontWeight: 'bold' }}>
             {dashboard.title}
           </div>
           <div
             onClick={() => onDelete(dashboard.id)}
             style={{
-              cursor: "pointer",
-              display: "inline-block",
+              cursor: 'pointer',
+              display: 'inline-block',
               marginLeft: 10,
-              padding: "0px 5px",
-              fontWeight: "bold",
+              padding: '0px 5px',
+              fontWeight: 'bold',
             }}
           >
             X
@@ -38,18 +43,18 @@ function DashboardsList(props: any) {
   const getAddDashboard = () => (
     <div
       style={{
-        display: "inline-block",
+        display: 'inline-block',
         width: 250,
         height: 100,
-        border: "3px dashed black",
+        border: '3px dashed black',
       }}
     >
       <div
         style={{
-          display: "inline-block",
+          display: 'inline-block',
           padding: 40,
-          cursor: "pointer",
-          fontWeight: "bold",
+          cursor: 'pointer',
+          fontWeight: 'bold',
         }}
         onClick={() => onAdd()}
       >
@@ -59,15 +64,22 @@ function DashboardsList(props: any) {
   );
 
   return (
-    <div style={{ margin: "auto", width: "100%", textAlign: "center" }}>
-      {getAddDashboard()}
-      {dashboards.map((dashboard: any) => getDashboard(dashboard))}
+    <div className={classes.body}>
+      <HeaderComponent
+        logoutSession={props.logoutSession}
+        user={user}
+        setOpenTask={setOpenTask}
+      />
+      <div style={{ margin: 'auto', width: '100%', textAlign: 'center' }}>
+        {getAddDashboard()}
+        {dashboards.map((dashboard: any) => getDashboard(dashboard))}
+      </div>
     </div>
   );
 }
 
 DashboardsList.defaultProps = {
-  dashboards: [{ title: "Dashboard1" }, { title: "Dashboard2" }],
+  dashboards: [{ title: 'Dashboard1' }, { title: 'Dashboard2' }],
   onClose: () => {},
   onAdd: () => {},
 };
